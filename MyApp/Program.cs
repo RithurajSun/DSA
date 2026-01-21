b2dfabf918757679c123c00f0990b73216c20844
Rithuraj <codesourcedev431@gmail.com>
2026-01-21 16:02:40 +0530

    Remove unused ReverseArray
    Deleted the ReverseArray method as it is no longer used in the codebase. This helps clean up the Program.cs file and maintain code clarity.

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -26,19 +26,6 @@ namespace MyApp
             }
             return (largest, smallest);
         }
-        public static void ReverseArray(int[] nums)
-        {
-            int left = 0;
-            int right = nums.Length - 1;
-            while (left < right)
-            {
-                int temp = nums[left];
-                nums[left] = nums[right];
-                nums[right] = temp;
-                left++;
-                right--;
-            }
-        }
         public static int SecondLargest(int[] nums)
         {
             int largest = int.MinValue;
