1e35e1806b24ea296cf6da5a22fea21b84a94068
Rithuraj <codesourcedev431@gmail.com>
2026-01-21 14:24:19 +0530

    Initial Commit

---
+++ MyApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace MyApp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 1, 2 };
+            var result = FindLargestAndSmallest(arr);
+            Console.WriteLine($" Largest : {result.a}, Smallest: {result.b}");
+        }
+        public static (int a, int b) FindLargestAndSmallest(int[] nums)
+        {
+            int smallest = nums[0];
+            int largest = nums[0];
+            foreach ( int num in nums)
+            {
+                if (num > largest)
+                {
+                    largest = num;
+                }
+                if (num < smallest)
+                {
+                    smallest = num;
+                }
+            }
+            return (largest, smallest);
+        }
+    }
+}
